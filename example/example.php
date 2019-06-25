e070690606260cb5b8f2bee28329195c3e10e018
eailyass <eailyass@mydailyexperience.com>
2019-06-25 15:32:22 +0100

    patch function replaceUser, add comments, lint code and run phpUnit

--- example/example.php
+++ example/example.php
@@ -19,16 +19,16 @@ $faker = \Faker\Factory::create();
 
 $template = new Template(
     1,
-    'Votre voyage avec une agence locale [quote:destination_name]',
+    'Votre voyage avec une agence locale [quote:destination_name] <br>',
     "
-Bonjour [user:first_name],
+Bonjour [user:first_name], <br>
 
 Merci d'avoir contacté un agent local pour votre voyage [quote:destination_name].
 
 Bien cordialement,
 
-L'équipe Evaneos.com
-www.evaneos.com
+L'équipe Evaneos.com 
+www.evaneos.com ==> [quote:destination_link]
 ");
 $templateManager = new TemplateManager();
 
--- src/TemplateManager.php
+++ src/TemplateManager.php
@@ -1,7 +1,13 @@
 <?php
 
+/*
+Le but est de rendre la classe plus lisible, pour cela il faut pensr à réamenagerla fonction computeText qui se montre un peu chargée en la divisant à plusieurs petites fonctions puis ajouter des commentaires pour rendre le tout plus lisible
+ */
+
+
 class TemplateManager
 {
+    // ne pas changer la signature de cette fonction
     public function getTemplateComputed(Template $tpl, array $data)
     {
         if (!$tpl) {
@@ -14,6 +20,8 @@ class TemplateManager
 
         return $replaced;
     }
+
+    // tenter de réduire la taille de cette fonction, elle doit servir à remplacer les valeurs mises entre quotes dans le texte en entrée par des valeurs de nos entités 
 
     private function computeText($text, array $data)
     {
@@ -30,39 +38,31 @@ class TemplateManager
          * USER
          * [user:*]
          */
-        $_user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $APPLICATION_CONTEXT->getCurrentUser();
-        if($_user) {
-            (strpos($text, '[user:first_name]') !== false) and $text = str_replace('[user:first_name]'       , ucfirst(mb_strtolower($_user->firstname)), $text);
-        }
+        $user  = (isset($data['user'])  and ($data['user']  instanceof User))  ? $data['user']  : $APPLICATION_CONTEXT->getCurrentUser();
+        if($user) 
+            $text= $this->replaceUser($text,$user);
 
         return $text;
     }
 
 
+    // fonction servant à remplacer les valeurs de l'entité quote dans le texte
 
-    private function replaceText($text, Quote $quote){
+    private function replaceText($text, Quote $quote)
+    {
 
-//        $quote = $data['quote'];
-
-        //$quoteFrom = QuoteRepository::getInstance()->getById($quote->id);
         $site = SiteRepository::getInstance()->getById($quote->siteId);
         $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
 
-       /* if(strpos($text, '[quote:destination_link]') !== false){
-            $destination = DestinationRepository::getInstance()->getById($quote->destinationId);
-        } //why?*/
 
-        $containsSummaryHtml = strpos($text, '[quote:summary_html]');
-        $containsSummary     = strpos($text, '[quote:summary]');
-
-        if ($containsSummaryHtml !== false) {
+        if (strpos($text, '[quote:summary_html]') !== false) {
             $text = str_replace(
                 '[quote:summary_html]',
                 Quote::renderHtml($quoteFrom),
                 $text
             );
         }
-        if ($containsSummary !== false) {
+        if (strpos($text, '[quote:summary]') !== false) {
             $text = str_replace(
                 '[quote:summary]',
                 Quote::renderText($quoteFrom),
@@ -70,17 +70,32 @@ class TemplateManager
             );
         }
 
-
         if(strpos($text, '[quote:destination_name]') !== false){
-            $text = str_replace('[quote:destination_name]',$destination->countryName,$text);
+            $text = str_replace(
+                '[quote:destination_name]',
+                $destination->countryName,
+                $text);
         }
 
         if (isset($destination))
-            $text = str_replace('[quote:destination_link]', $site->url . '/' . $destination->countryName . '/quote/' . $quote->id, $text);
+            $text = str_replace('[quote:destination_link]',
+            $site->url . '/' . $destination->countryName . '/quote/' . $quote->id,
+            $text);
         else
             $text = str_replace('[quote:destination_link]', '', $text);
 
         return $text;
     
+    }
+
+    // fonction servant à remplacer les valeurs de l'entité User dans le texte
+
+    private function replaceUser($text, User $user)
+    {
+        if(strpos($text, '[user:first_name]') !== false)
+         $text = str_replace('[user:first_name]',
+         ucfirst(mb_strtolower($user->firstname)),
+         $text);
+        return $text;
     }
 }
